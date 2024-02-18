12064c762b84dac8a4d2a1a007c6eb73cfc6c826
Harsh panwar <panwarharshwardhan@email.com>
2024-02-18 15:48:08 +0530

    add playlist controller

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -8,20 +8,121 @@ const createPlaylist = asyncHandler(async (req, res) => {
   const { name, description } = req.body;
 
   //TODO: create playlist
+  if (!name) {
+    throw new ApiError(400, "Invalid playlist name");
+  }
+
+  const playlist = await Playlist.create({
+    name,
+    description,
+    owner: req.user._id,
+  });
+  if (!playlist) {
+    throw new ApiError(500, "server error creating playlist failed");
+  }
+  return res
+    .status(200)
+    .json(new ApiResponse(200, playlist, "success create playlist"));
 });
 
 const getUserPlaylists = asyncHandler(async (req, res) => {
   const { userId } = req.params;
   //TODO: get user playlists
+  if (!isValidObjectId(userId)) {
+    throw new ApiError(400, "Invalid userId");
+  }
+  const userPlaylists = await Playlist.aggregate([
+    {
+      $match: {
+        owner: mongoose.Types.ObjectId(userId),
+      },
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "videos",
+        foreignField: "_id",
+        as: "playlist",
+      },
+    },
+  ]);
+  if (!userPlaylists) {
+    throw new ApiError(500, "intrnal server error Playlists not found");
+  }
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, userPlaylists, "playlists successfully fetched")
+    );
 });
 
 const getPlaylistById = asyncHandler(async (req, res) => {
   const { playlistId } = req.params;
   //TODO: get playlist by id
+  if (!isValidObjectId(playlistId)) {
+    throw new ApiError(400, "playlist id required");
+  }
+  const userPlaylist = await Playlist.aggregate([
+    {
+      $match: {
+        _id: mongoose.Types.ObjectId(playlistId),
+      },
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "videos",
+        foreignField: "_id",
+        as: "playlist",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+            },
+          },
+          {
+            $project: {
+              fullName: 1,
+              userName: 1,
+              avatar: 1,
+            },
+          },
+        ],
+      },
+    },
+  ]);
+  if (!userPlaylist) {
+    throw new ApiError(500, "intrnal server error Playlist not found");
+  }
+  return res
+    .status(200)
+    .json(new ApiResponse(200, userPlaylist, "playlists successfully fetched"));
 });
 
 const addVideoToPlaylist = asyncHandler(async (req, res) => {
   const { playlistId, videoId } = req.params;
+  if (!isValidObjectId(playlistId)) {
+    throw new ApiError(400, "playlist id required");
+  }
+  if (!isValidObjectId(videoId)) {
+    throw new ApiError(400, "video id required");
+  }
+  const userPlaylist = await Playlist.findById(playlistId);
+  if (!userPlaylist) {
+    throw new ApiError(500, "intrnal server error Playlist not found");
+  }
+  userPlaylist.videos.push(videoId);
+  await userPlaylist.save({ validateBeforeSave: false });
+  const updatedPlaylist = await Playlist.findById(playlistId);
+  if (userPlaylist.videos.length() === updatedPlaylist.videos.length()) {
+    throw new ApiError(500, "video not add in playlist");
+  }
+  return res
+    .status(200)
+    .json(new ApiResponse(200, updatedPlaylist, "successfully add video"));
 });
 
 const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
